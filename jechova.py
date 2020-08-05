dff480be6984887ceb52a7ffcd656b2607cf5b83
Honza Javorek <mail@honzajavorek.cz>
2020-08-05 15:39:54 +0200

    use args directly

--- jechova.py
+++ jechova.py
@@ -19,11 +19,8 @@ if __name__ == '__main__':
     parser.add_argument('channel')
     parser.add_argument('ics_url')
     args = parser.parse_args()
-    force = args.force
-    channel = args.channel.lstrip('#')
-    ics_url = args.ics_url
 
-    response = requests.get(ics_url)
+    response = requests.get(args.ics_url)
     response.raise_for_status()
     calendar = Calendar(response.text)
 
@@ -32,17 +29,19 @@ if __name__ == '__main__':
                 key=attrgetter('begin'))
     remaining_days = (event.begin - today).days
 
-    if not force and remaining_days not in NOTIFY_WHEN_REMAINING_DAYS:
+    if not args.force and remaining_days not in NOTIFY_WHEN_REMAINING_DAYS:
         print('Dní do dalšího srazu:', remaining_days)
-        print('Ozývám se, když zbývá:', ', '.join(map(str, NOTIFY_WHEN_REMAINING_DAYS)))        
+        print('Ozývám se, když zbývá:',
+              ', '.join(map(str, NOTIFY_WHEN_REMAINING_DAYS)))
         sys.exit()
 
     if 'tentative-date' not in event.categories:
         print("Zdá se, že příští sraz je naplánovaný!")
         sys.exit()
 
+    channel = f'#{args.channel.lstrip('#')}'
     client = WebClient(token=os.environ['SLACK_API_TOKEN'])
-    client.chat_postMessage(channel=f'#{channel}', text=dedent('''
+    client.chat_postMessage(channel=channel, text=dedent('''
         Máte téma? A mohla bych ho vidět? :eyes:\n\n
         Pokud už tušíte, o čem Pyvo bude, přidejte to na pyvo.cz — může
         to udělat kdokoliv, v 5 jednoduchých krocích:
