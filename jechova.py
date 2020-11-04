6b37cbb38e5344ffc5eff789ef1daab3f5b1879e
Petr Viktorin <encukou@gmail.com>
2020-11-04 21:06:54 +0100

    Announce remaining days and adapt the emoji
    Fixes: https://github.com/pyvec/jechova/issues/5

--- jechova.py
+++ jechova.py
@@ -9,12 +9,20 @@ import requests
 from slack import WebClient
 
 
-NOTIFY_WHEN_REMAINING_DAYS = (14, 7, 3)
+# Only send a message when the number of remaining days is a key here.
+# Values are emoji names; they should be more urgent for smaller numbers.
+NOTIFY_WHEN_REMAINING_DAYS = {
+    3: 'panicmonster',
+    7: 'hourglass_flowing_sand',
+    14: 'eyes'
+}
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--force', '-f', action='store_true')
+    parser.add_argument('--today', metavar='YYYY-MM-DD',
+                        help="Override current date (for testing)")
     parser.add_argument('channel')
     parser.add_argument('pyvo_slug')
     args = parser.parse_args()
@@ -28,7 +36,10 @@ if __name__ == '__main__':
     response.raise_for_status()
     calendar = Calendar(response.text)
 
-    today = arrow.now('Europe/Prague')
+    if args.today:
+        today = arrow.get(args.today)
+    else:
+        today = arrow.now('Europe/Prague')
     event = min((e for e in calendar.events if e.begin > today),
                 key=attrgetter('begin'))
     remaining_days = (event.begin - today).days
@@ -39,6 +50,11 @@ if __name__ == '__main__':
               ', '.join(map(str, NOTIFY_WHEN_REMAINING_DAYS)))
         sys.exit()
 
+    for days, emoji in sorted(NOTIFY_WHEN_REMAINING_DAYS.items()):
+        if remaining_days <= days:
+            break
+        # nb. `emoji` keeps the value from the last iteration of this loop
+
     if 'tentative-date' not in event.categories:
         print("Zdá se, že příští sraz je naplánovaný!")
         sys.exit()
@@ -48,8 +64,20 @@ if __name__ == '__main__':
 
     docs_url = "https://docs.pyvec.org/guides/meetup.html#informace-na-pyvo-cz"
     web_url = f'https://pyvo.cz/{args.pyvo_slug}/'
+
+    if remaining_days < 0:
+        remaining_msg = 'Pyvo už bylo!'
+    elif remaining_days == 0:
+        remaining_msg = 'Pyvo je dnes!'
+    elif remaining_days == 1:
+        remaining_msg = 'Zbývá poslední den!'
+    elif remaining_days < 5:
+        remaining_msg = f'Zbývají {remaining_days} dny'
+    else:
+        remaining_msg = f'Zbývá {remaining_days} dní'
+
     client.chat_postMessage(channel=channel, text=' '.join((
-        'Máte téma? A mohla bych ho vidět? :eyes:\n',
+        f'Máte téma? A mohla bych ho vidět? {remaining_msg} :{emoji}:\n',
         'Pokud už tušíte, o čem Pyvo bude, přidejte to na pyvo.cz',
         'přes odkaz _přidat informace o dalším srazu_ na',
         f'<{web_url}|pyvo.cz/{args.pyvo_slug}>.\n',
