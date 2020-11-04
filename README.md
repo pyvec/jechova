369b41697c46945fc6174494552f0d4ff80a5525
Petr Viktorin <encukou@gmail.com>
2020-11-04 21:14:32 +0100

    Advertise an easier way to add meetups
    This needs an API change: we take a slug rather than full URL
    to the calendar, so that an URL for the meetup can be generated.
    The old way will raise an exception.
    
    Also, streamline the message.
    Slack respects newlines, so textwrap.dedent(), which keeps newlines,
    isn't the best tool for the job.
    
    https://github.com/pyvec/jechova/pull/16
    
    Co-authored-by: Honza Javorek <mail@honzajavorek.cz>

--- README.md
+++ README.md
@@ -12,12 +12,12 @@ Checks whether the next meetup is announced on [pyvo.cz](https://pyvo.cz/). If n
 
 ```bash
 $ export SLACK_API_TOKEN=...
-$ python jechova.py '#pyvo-praha' https://pyvo.cz/api/series/praha-pyvo.ics
+$ python jechova.py '#pyvo-praha' praha-pyvo
 ```
 
 First argument is a channel name where the message should go. Beware, if you specify it as `#pyvo-praha`, standard shell interprets the `#` as a start of a comment and will ignore the rest of the line. You must pass it either as `pyvo-praha` or as `'#pyvo-praha'` (both works).
 
-Second argument is the source ics feed for the Pyvo meetup you want to watch. Each meetup tracked on pyvo.cz has the ics feed automatically generated, you can find a link somewhere on the meetup page (e.g. somewhere on [pyvo.cz/praha-pyvo](https://pyvo.cz/praha-pyvo/)).
+Second argument is the meetup slug used in the meetup URL. For example, if the meetup URL is `https://pyvo.cz/praha-pyvo/`, the slug would be `praha-pyvo`.
 
 When debugging, force sending of the message by adding `-f`. (And use `#automatizace` or other less populated channel.)
 
--- jechova.py
+++ jechova.py
@@ -2,7 +2,6 @@ from argparse import ArgumentParser
 import os
 import sys
 from operator import attrgetter
-from textwrap import dedent
 
 import arrow
 from ics import Calendar
@@ -17,10 +16,15 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--force', '-f', action='store_true')
     parser.add_argument('channel')
-    parser.add_argument('ics_url')
+    parser.add_argument('pyvo_slug')
     args = parser.parse_args()
 
-    response = requests.get(args.ics_url)
+    if '/' in args.pyvo_slug:
+        raise ValueError(
+            "Meetup slug cannot contain slashes. See the README for usage."
+        )
+
+    response = requests.get(f'https://pyvo.cz/api/series/{args.pyvo_slug}.ics')
     response.raise_for_status()
     calendar = Calendar(response.text)
 
@@ -41,9 +45,13 @@ if __name__ == '__main__':
 
     channel = '#' + args.channel.lstrip('#')
     client = WebClient(token=os.environ['SLACK_API_TOKEN'])
-    client.chat_postMessage(channel=channel, text=dedent('''
-        Máte téma? A mohla bych ho vidět? :eyes:\n\n
-        Pokud už tušíte, o čem Pyvo bude, přidejte to na pyvo.cz — může
-        to udělat kdokoliv, v 5 jednoduchých krocích:
-        https://docs.pyvec.org/guides/meetup.html#informace-na-pyvo-cz
-    ''').strip())
+
+    docs_url = "https://docs.pyvec.org/guides/meetup.html#informace-na-pyvo-cz"
+    web_url = f'https://pyvo.cz/{args.pyvo_slug}/'
+    client.chat_postMessage(channel=channel, text=' '.join((
+        'Máte téma? A mohla bych ho vidět? :eyes:\n',
+        'Pokud už tušíte, o čem Pyvo bude, přidejte to na pyvo.cz',
+        'přes odkaz _přidat informace o dalším srazu_ na',
+        f'<{web_url}|pyvo.cz/{args.pyvo_slug}>.\n',
+        '(Složitější případy řešte v <#C97Q9HHHT>)',  # links to #pyvo channel
+    )))
